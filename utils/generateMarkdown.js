ffc6d61618cc128bd020f48ff531dd9a5d737126
Biz <github@bisrat.dev>
2023-04-13 16:37:14 -0600

    write to file and licensing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,27 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const badgeRefs = {
+    Apache_20: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    BSL_10:'[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+    BSD_3_Clause: '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+    BSD_2_Clause: '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)',
+    Creative_Commons_0_10: '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)',
+    GNU_GPLv3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    IBM_Public_License_Version_10: '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)',
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    none: '![License: none](https://img.shields.io/badge/license-none-informational)'
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+    license = license.replace(/\s/g,'_').replace(/\./g,'');
+    return badgeRefs[license];
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    return `## License ${renderLicenseBadge(license)}\n${license == 'none' ? 'This project is not licensed  ' : `Licensed ${license}  `}\n`
+}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+    data['install'] = data['installmethod'] === 'bash' ? `\`\`\`bash\n${data['install']}\n\`\`\`` : data['install'];
+    return `# ${data['title']}\n${data['description']}\n## Installation\n${data['install']}\n___\n## Usage\n${data['use']}\n___\n## Testing\n${data['testing']}\n___\n## Contributing\n${data['contributing']}\n___\n${renderLicenseSection(data['license'])}\n`
 }
 
 module.exports = generateMarkdown;
